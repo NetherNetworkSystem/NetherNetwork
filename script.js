5d8ea21669cddff96fe13ee5cbf79e8a51bf5117
NetherNetworkSystem <96076582+NetherNetworkSystem@users.noreply.github.com>
2021-12-13 18:42:17 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+
+let wrapper = document.getElementById('wrapper');
+wrapper.addEventListener('mousemove', (event) => {
+    let moveX = ((window.screen.width / 2) - event.pageX) * 0.005;
+    let moveY = ((window.screen.height / 2) - event.pageY) * 0.005;
+    let background = document.getElementById('background');
+    background.style.marginLeft = moveX + 'px';
+    background.style.marginTop = moveY + 'px';
+});
+
