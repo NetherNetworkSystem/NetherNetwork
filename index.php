3f60040c0920c26a6436820aef6c2358df0f5abe
NetherNetworkSystem <96076582+NetherNetworkSystem@users.noreply.github.com>
2021-12-13 18:38:04 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="hu">
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>NetherNetwork</title>
+    <link rel="stylesheet" href="./assets/style.css">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.4.2/css/all.css">
+    <link rel="icon" href="logo.png">
+</head>
+<body>
+    <div class="wrapper" id="wrapper">
+        <div class="background" id="background" style="margin-left: -6.58px; margin-top: 2.62px;"></div>
+        <div class="particles" id="particles"><canvas class="particles-js-canvas-el" style="width: 100%; height: 100%;"
+                width="1280" height="573"></canvas></div>
+        <div class="logo title-text" style="color:white;">
+            <center><img src="logo.png"></center>
+            <br><div class="text">NetherNetwork</div>
+        </div>
+        <div class="buttons" style="margin-top:10%;">
+            <a class="button" href="#">
+                <span class="icon">
+                    <i class="fas fa-store"></i>
+                </span>
+                <span class="text">
+                    Bolt
+                </span>
+            </a>
+            <a class="button" href="https://discord.gg/YYYMFBKzd2">
+                <span class="icon">
+                    <i class="fab fa-discord"></i>
+                </span>
+                <span class="text">
+                    Discord
+                </span>
+            </a>
+            <a class="button" href="#">
+                <span class="icon">
+                    <i class="fas fa-gavel"></i>
+                </span>
+                <span class="text">
+                    Szabályzat
+                </span>
+            </a>
+        </div>
+    </div>
+    <script async src="./assets/particles.min.js" type="1187eb4c05e28e88e8a856d6-text/javascript"></script>
+    <script async src="./assets/script.js" type="1187eb4c05e28e88e8a856d6-text/javascript"></script>
+    <script async src="./assets/rocket-loader.min.js" data-cf-settings="1187eb4c05e28e88e8a856d6-|49" defer=""></script>
+</body>
+
+</html>
